f4984a7870060bf56f9193f64a2c220717212e99
Kaue Miziara <kauemiziara@gmail.com>
2024-12-02 17:09:19 -0300

    Day 02

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,7 @@ import Day02
 
 main :: IO ()
 main = do
-  (p1, p2) <- solveDay02 "src/day02test.txt"
+  -- (p1, p2) <- solveDay02 "src/day02test.txt"
+  (p1, p2) <- solveDay02 "input/day02.txt"
   putStrLn $ "Part 1: " ++ show p1
   putStrLn $ "Part 2: " ++ show p2
--- src/Day02.hs
+++ src/Day02.hs
@@ -1,6 +1,6 @@
 module Day02 (solveDay02) where
 
-import Utils (readFileLines, isOrdered)
+import Utils (isOrdered, readFileLines)
 
 solveDay02 :: FilePath -> IO (Int, Int)
 solveDay02 filePath = do
@@ -9,20 +9,32 @@ solveDay02 filePath = do
       resultP2 = solveDay02p2 input
   return (resultP1, resultP2)
 
-
 solveDay02p1 :: [[Int]] -> Int
 solveDay02p1 = length . filter (\report -> isOrdered report && isSafe report)
 
 solveDay02p2 :: [[Int]] -> Int
-solveDay02p2 _ = 0
-
+solveDay02p2 = length . filter isSafeDampener
 
 isSafe :: [Int] -> Bool
 isSafe [] = True
 isSafe [_] = True
-isSafe (x:y:xs) = 
+isSafe (x : y : xs) =
   let diff = abs (y - x)
-  in diff >= 1 && diff <= 3 && isSafe (y:xs)
+   in diff >= 1 && diff <= 3 && isSafe (y : xs)
+
+isSafeDampener :: [Int] -> Bool
+isSafeDampener report
+  | isOrdered report && isSafe report = True
+  | otherwise =
+      any
+        ( \i ->
+            let reduced = removeAt i report
+             in isOrdered reduced && isSafe reduced
+        )
+        [0 .. length report - 1]
+  where
+    removeAt i xs = take i xs ++ drop (i + 1) xs
+
 
 parseInput :: FilePath -> IO [[Int]]
 parseInput filePath = do
