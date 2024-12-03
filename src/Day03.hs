cf4ee6d7056cea8deaf220e5cbdb9a0165bde0bb
Kaue Miziara <kauemiziara@gmail.com>
2024-12-03 19:39:32 -0300

    Day 03

--- src/Day03.hs
+++ src/Day03.hs
@@ -14,8 +14,15 @@ solveDay03p1 :: [String] -> Int
 solveDay03p1 input = sum $ map (sum . map multiply . extractMul) input
 
 solveDay03p2 :: [String] -> Int
-solveDay03p2 _ = 0
+solveDay03p2 = fst . foldl processHelper (0, True)
+  where
+    processHelper (totalSum, currentState) line =
+      let (lineSum, newState) = handleState line currentState
+       in (totalSum + lineSum, newState)
 
+
+multiply :: (Int, Int) -> Int
+multiply (x, y) = x * y
 
 extractMul :: String -> [(Int, Int)]
 extractMul [] = []
@@ -29,5 +36,19 @@ extractMul ('m' : 'u' : 'l' : '(' : rest) =
     _ -> extractMul rest
 extractMul (_ : rest) = extractMul rest
 
-multiply :: (Int, Int) -> Int
-multiply (x, y) = x * y
+handleState :: String -> Bool -> (Int, Bool)
+handleState [] state = (0, state)
+handleState ('d' : 'o' : '(' : ')' : rest) _ = handleState rest True
+handleState ('d' : 'o' : 'n' : '\'' : 't' : '(' : ')' : rest) _ = handleState rest False
+handleState ('m' : 'u' : 'l' : '(' : rest) True =
+  case span isDigit rest of
+    (num1, ',' : rest') ->
+      case span isDigit rest' of
+        (num2, ')' : rest'') ->
+          let (sumRest, finalState) = handleState rest'' True
+           in (read num1 * read num2 + sumRest, finalState)
+        _ -> handleState rest' True
+    _ -> handleState rest True
+handleState ('m' : 'u' : 'l' : '(' : rest) False =
+  handleState (dropWhile (/= ')') rest) False
+handleState (_ : rest) state = handleState rest state
