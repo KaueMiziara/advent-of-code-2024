013de92ca11e0ec4f6375774c955836597a6803d
Kaue Miziara <kauemiziara@gmail.com>
2024-12-21 21:00:14 -0300

    Day 04 p1

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,7 @@ import Day04
 
 main :: IO ()
 main = do
-  (p1, p2) <- solveDay04 "src/itest.txt"
-  -- (p1, p2) <- solveDay03 "input/day04.txt"
+  -- (p1, p2) <- solveDay04 "src/itest.txt"
+  (p1, p2) <- solveDay04 "input/day04.txt"
   putStrLn $ "Part 1: " ++ show p1
   putStrLn $ "Part 2: " ++ show p2
--- src/Day04.hs
+++ src/Day04.hs
@@ -10,7 +10,24 @@ solveDay04 filePath = do
   return (resultP1, resultP2)
 
 solveDay04p1 :: [String] -> Int
-solveDay04p1 _ = 0
+solveDay04p1 grid = length $ findAllOccurrences grid "XMAS"
 
 solveDay04p2 :: [String] -> Int
 solveDay04p2 _ = 0
+
+findAllOccurrences :: [String] -> String -> [(Int, Int, (Int, Int))]
+findAllOccurrences grid word = 
+  [(x, y, dir) | x <- [0 .. rows - 1], y <- [0 .. cols - 1], dir <- directions, match grid word x y dir]
+  where
+    rows = length grid
+    cols = length (head grid)
+    directions = [(1, 0), (0, 1), (1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1), (-1, 1)]
+
+match :: [String] -> String -> Int -> Int -> (Int, Int) -> Bool
+match grid word x y (dx, dy) =
+  all (\(i, c) -> inBounds (x + i * dx) (y + i * dy) && grid !! (x + i * dx) !! (y + i * dy) == c) indexedWord
+  where
+    indexedWord = zip [0 ..] word
+    rows = length grid
+    cols = length (head grid)
+    inBounds i j = i >= 0 && i < rows && j >= 0 && j < cols
